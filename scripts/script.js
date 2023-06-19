8f012483dcf216db23d62d9e0e29797c3db7be4b
Patrick Dunne <134411981+pjsdunne@users.noreply.github.com>
2023-06-19 12:48:56 +0100

    Update script.js to add Open Now functionality

--- scripts/script.js
+++ scripts/script.js
@@ -28,3 +28,62 @@ function myFunction(imgs) {
 }
   expandImg.parentElement.style.display = "block";
 }
+
+
+
+function OpenNow(){
+	let isToday = new Date();
+	var currentDay = isToday.getDay();
+	var currentTime = isToday.getHours() + ":" + isToday.getMinutes();
+	var currentMonth = isToday.getMonth();
+	console.log (currentMonth);
+	var opennowtext = document.getElementById("isopennow");
+	var start;
+	var end;
+	switch(currentDay){
+		case 0:
+			start = "08:00";
+			end = "21:00";
+			break;
+		case 1:
+			start = "06:00";
+			end = "22:00";
+			break;
+		case 2:
+			start = "06:00";
+			end = "22:00";
+			break;
+		case 3:
+			start = "06:00";
+			end = "22:00";
+			break;
+		case 4:
+			start = "06:00";
+			end = "22:00";
+			break;
+		case 5:
+			start = "06:00";
+			end = "22:00";
+			break;
+		case 6:
+			start = "08:00";
+			end = "21:00";
+			break;
+		
+	}
+	
+	if (currentTime >= start && currentTime <= end) {
+			opennowtext.innerHTML = "The gym is open right now.";
+	} else {
+			opennowtext.innerHTML = "Sorry, the gym is closed right now.";
+	}
+	
+	if (currentDay == 1){
+			opennowtext.innerHTML = opennowtext.innerHTML + "<br>Please note our Bank Holiday opening times."
+	}
+	
+	if (currentMonth == 11){
+			opennowtext.innerHTML = opennowtext.innerHTML + "<br>Christmas opening times may vary. Closed Christmas Day and Stephens Day."
+	}
+
+}
